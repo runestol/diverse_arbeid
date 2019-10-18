9e71fa9035f54e22bf32b4328de76efca3162ae5
runestol <runestol@student.matnat.uio.no>
2019-10-18 14:19:04 +0200

    sletta

--- datastrukturar/run.sh
+++
@@ -1,8 +0,0 @@
-c++ array_list.cpp -Wall -o array_list.x -std=c++11
-./array_list.x
-
-c++ linked_list.cpp -Wall -o linked_list.x -std=c++11
-./linked_list.x
-
-c++ circular_linked_list.cpp -Wall -o circular_linked_list.x -std=c++11
-./circular_linked_list.x
